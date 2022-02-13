35d8dcf29534edeb3bcfdf44acb9b7fae7e68bb5
th1237 <zzzatt2@gmail.com>
2022-02-13 22:37:41 +0900

    Add Object.java to study cases for Object Array Friend

---
+++ src/main/java/com/example/study_object/objectarray/friend/FriendMain.java
@@ -0,0 +1,10 @@
+package com.example.study_object.objectarray.friend;
+
+public class FriendMain {
+
+    public static void main(String[] args) {
+        FriendManager fm = new FriendManager();
+        fm.menu();
+    }
+
+}
