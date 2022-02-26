76b0d33e3bb718a46867a297d932ab10e732f946
th1237 <zzzatt2@gmail.com>
2022-02-26 23:41:28 +0900

    Add collections.java to study cases for ArrayList Student Example

---
+++ src/main/java/com/example/collections/Student.java
@@ -0,0 +1,38 @@
+package com.example.collections;
+
+public class Student {
+
+    private int no;
+    private String name;
+
+    public Student() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public Student(int no, String name) {
+        super();
+        this.no = no;
+        this.name = name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Student [no=" + no + ", name=" + name + "]";
+    }
+}
