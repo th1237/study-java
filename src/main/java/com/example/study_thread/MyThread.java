7e08e3a242d1c5dbfb13395966c24a6b926f8f3c
th1237 <zzzatt2@gmail.com>
2022-03-08 22:41:28 +0900

    Add thread.java to study MyThread

---
+++ src/main/java/com/example/study_thread/MyThread.java
@@ -0,0 +1,22 @@
+package com.example.study_thread;
+
+public class MyThread extends Thread {
+
+    char ch;
+
+    public MyThread(char ch) {
+        this.ch = ch;
+    }
+
+    /**
+     * 쓰레드의 작업내용
+     */
+    @Override
+    public void run() {
+
+        for(int i = 0; i < 100; i++)
+            System.out.print(ch);
+
+    }
+}
+
