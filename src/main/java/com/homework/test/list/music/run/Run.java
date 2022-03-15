6e09867a421c301d866b0c366d8d83964574d111
th1237 <zzzatt2@gmail.com>
2022-03-15 22:29:15 +0900

    Add collections.java to Test list Music

---
+++ src/main/java/com/homework/test/list/music/run/Run.java
@@ -0,0 +1,11 @@
+package com.homework.test.list.music.run;
+
+import com.homework.test.list.music.view.MusicMenu;
+
+public class Run {
+
+    public static void main(String[] args) {
+        new MusicMenu().mainMenu();
+        System.out.println("----- 이용해주셔서 감사합니다. -----");
+    }
+}
