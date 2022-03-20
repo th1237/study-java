566008572eb7df6f28cf7619912dc6330ce66a81
th1237 <zzzatt2@gmail.com>
2022-03-20 20:35:33 +0900

    Add WorkShop.java to Test WorkShop

---
+++ src/main/java/com/homework/test/workshop/Test02.java
@@ -0,0 +1,13 @@
+package com.homework.test.workshop;
+
+public class Test02 {
+
+    public static void main(String[] args) {
+        for(int inx = 0; inx <= 6; inx++) {
+            for(int jnx = 0; jnx <= inx; jnx++) {
+                System.out.print("*");
+            }
+            System.out.print("@");
+        }
+    }
+}
