e0954ee396c727199a7662daa003c6765e0cd12a
th1237 <zzzatt2@gmail.com>
2022-03-22 14:38:24 +0900

    Add exception.java to Test NumberProcess

---
+++ src/main/java/com/homework/test/exception/number/NumberProcess.java
@@ -0,0 +1,18 @@
+package com.homework.test.exception.number;
+
+public class NumberProcess {
+
+    public NumberProcess() {
+        super();
+    }
+
+    public boolean checkDouble(int a, int b) {
+        if (a < 1 || a > 100 || b < 1 || b > 100)
+            throw new NumberRangeException("1부터 100사이의 값이 아닙니다.");
+
+        if (a % b == 0)
+            return true;
+        else
+            return false;
+    }
+}
