b303f1739c6a88c89f693b8503732d6869f4be2e
th1237 <zzzatt2@gmail.com>
2022-03-29 20:23:26 +0900

    Add Workshop.java to Test Mobile

---
+++ src/main/java/com/homework/test/workshop/Ltab.java
@@ -0,0 +1,18 @@
+package com.homework.test.workshop;
+
+public class Ltab extends Mobile {
+    public Ltab() {}
+    public Ltab(String mobileName, int batterySize, String osType) {
+        super(mobileName, batterySize, osType);
+    }
+    @Override
+    public int opertate(int time) {
+        super.setBatterySize(super.getBatterySize() - time*10);
+        return super.getBatterySize();
+    }
+    @Override
+    public int charge(int time) {
+        super.setBatterySize(super.getBatterySize() + time*10);
+        return super.getBatterySize();
+    }
+}
