53c23a431fddfdc06e49f7e0949acaea496a2c25
th1237 <zzzatt2@gmail.com>
2022-03-30 20:35:51 +0900

    Add Workshop.java to Test Workshop

---
+++ src/main/java/com/homework/test/workshop/T3.java
@@ -0,0 +1,9 @@
+package com.homework.test.workshop;
+
+public class T3 {
+    public static void main(String[] args) {
+        String str = "LGcns";
+        System.out.printf("%s\n%s\n%s\n%s\n",str.toUpperCase(),str.toLowerCase(),
+                str.substring(0, 2).toUpperCase(),str.substring(2, 5).toLowerCase());
+    }
+}
