7d3dd0041aaf4db02a02908294f16311128f59d4
th1237 <zzzatt2@gmail.com>
2022-04-05 21:03:58 +0900

    Add jdbc.java to Statement

---
+++ src/main/java/com/example/study_jdbc/member/controller/MemberController.java
@@ -0,0 +1,53 @@
+package com.example.study_jdbc.member.controller;
+
+import java.util.List;
+
+/**
+ * controller클래스
+ * - 신호등 역활
+ * - mvc흐름에서
+ */
+
+import com.example.study_jdbc.member.model.dao.MemberDao;
+import com.example.study_jdbc.member.model.vo.Member;
+
+public class MemberController {
+
+    private MemberDao memberDao = new MemberDao();
+
+    /**
+     * 사용자의 회원가입 요청을 처리하는 메소드
+     *  1. dao에 회원가입 요청 후 int반환
+     *  2. menu에 int반환
+     */
+    public int insertMember(Member newMember) {
+        int result = memberDao.insertMember(newMember);
+        System.out.println("result@MemberController = " + result); // 로깅용 출력
+        return result;
+    }
+
+    public List<Member> selectAll() {
+        List<Member> list = memberDao.selectAll();
+        return list;
+    }
+
+    public Member selectOne(String id) {
+        Member member = memberDao.selectOne(id);
+        return member;
+    }
+
+    public List<Member> selectMulti(String name) {
+        List<Member> list = memberDao.selectMulti(name);
+        return list;
+    }
+
+    public int updateMember(Member member) {
+        int result = memberDao.updateMember(member);
+        return result;
+    }
+
+    public int deleteMember(String id) {
+        int result = memberDao.deleteMember(id);
+        return result;
+    }
+}
